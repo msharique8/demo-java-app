60f1044786d433ae94114f08a8ef51a307ae925c
Ansari <Mohammad.Ansari@searshc.com>
2019-06-26 13:21:53 +0530

    First commit

---
+++ src/main/java/com/demo/java/app/App.java
@@ -0,0 +1,13 @@
+package com.demo.java.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Curretent version is V1.1" );
+    }
+}
