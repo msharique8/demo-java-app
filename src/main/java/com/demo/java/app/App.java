ba35f54a34c64a341a1a05762eef2341dfaecc36
Ansari <Mohammad.Ansari@searshc.com>
2019-06-27 17:04:32 +0530

    Updated to version 1.3

--- src/main/java/com/demo/java/app/App.java
+++ src/main/java/com/demo/java/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Curretent version is V1.2" );
+        System.out.println( "Hello World! Curretent version is V1.3" );
     }
 }
